2308dda590f3658a806fca9dbe50aea5aa0c41bb
mjwebb1993 <mjwebb1993@yahoo.com>
2022-03-02 09:47:34 -0600

    stateless SPA complete

---
+++ components/Header.js
@@ -0,0 +1,7 @@
+import html from "html-literal";
+
+export default () => html`
+  <header>
+    <h1>Example Pre-SPA Multi-Page Site</h1>
+  </header>
+`;
---
+++ index.js
@@ -0,0 +1,27 @@
+import { Header, Nav, Main, Footer } from "./components";
+
+function render() {
+  document.querySelector("#root").innerHTML = `
+      ${Header()}
+      ${Nav()}
+      ${Main()}
+      ${Footer()}
+    `;
+}
+
+render();
+
+// add menu toggle to bars icon in nav bar
+document.querySelector(".fa-bars").addEventListener("click", () => {
+  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+});
+
+// handle form submission
+document.querySelector("form").addEventListener("submit", event => {
+  event.preventDefault();
+  Array.from(event.target.elements).forEach(el => {
+    console.log("Input Type: ", el.type);
+    console.log("Name: ", el.name);
+    console.log("Value: ", el.value);
+  });
+});
